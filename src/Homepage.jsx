ecad1d4f3c400c8f30512f89078510aa5080483e
Kitrinos018 <nellie.leaverton@gmail.com>
2023-11-02 18:27:16 -0500

    Stuck on using the Navbar. I have implemented the children path but it does nothing

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,8 +1,8 @@
 
-//Homepage Function
+//HomePage Function
 function HomePage() {
     return (
-      <div id="Homepage">
+      <div id="HomePage">
         <h1>Welcome to My Website</h1>
         <p>This is the homepage of my website. It's a simple example to get you started.</p>
         <p>Feel free to explore and navigate through the pages.</p>
@@ -12,4 +12,4 @@ function HomePage() {
   }
 
 //returning the function Navbar
-export default Homepage;
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import HomePage from "./HomePage"; //imports the HomePage
 import Navbar from "./Navbar/Navbar";
 
 const router = createBrowserRouter([{
     path: "/",
     element: <Navbar />,
+    //for somereason this breaks the website, im not sure why
+    children: [
+      {
+        path: "HomePage/:HomePage",
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
 
