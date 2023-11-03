a405b574d5d1ad4f9a44e64999964ddab6734389
Kitrinos018 <nellie.leaverton@gmail.com>
2023-11-02 19:52:00 -0500

    kinda working

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,18 @@
-//Imports//
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
-const router = createBrowserRouter([{
+const router = createBrowserRouter([
+  {
     path: "/",
-    element: <Navbar/>,
-    //for somereason this breaks the website, im not sure why
-    /*
-    children: [
-      {
-        path: "HomePage/:HomePageId",
-        element: <HomePage />,
-      },
-    ],
-    */
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
-//dont delete
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
---
+++ src/routes/root.jsx
@@ -0,0 +1,10 @@
+export default function Root() {
+    return (
+        <div class="topnav">
+        <a href="http://localhost:5173/src/root.jsx">Home</a>
+        <a href="http://localhost:5173/src/Attractions.html">Attractions</a>
+        <a href="http://localhost:5173/src/Food&Drink.html">Food & Drink</a>
+        <a href="http://localhost:5173/src/Contact.html">Contact Us</a>
+  </div>
+    );
+  }
