df07887db004de52513da51280ee60830c8d3aa2
sophiew <sophiew@iastate.edu>
2023-11-02 20:45:56 -0500

    Fixed serving the homepage!

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,22 @@ import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Home from "./routes/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children:[
+          {
+          path: "/home",
+          element: <Home />,
+        },],
+  },],
   },
 ]);
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,12 @@
 export default function Root() {
+
+
     return (
         <div class="topnav">
-        <a href="http://localhost:5173/src/root.jsx">Home</a>
-        <a href="http://localhost:5173/src/Attractions.html">Attractions</a>
-        <a href="http://localhost:5173/src/Food&Drink.html">Food & Drink</a>
-        <a href="http://localhost:5173/src/Contact.html">Contact Us</a>
+        <a href="/home">Home</a>
+        <a href="/attractions">Attractions</a>
+        <a href="http://localhost:5173/food.jsx">Food & Drink</a>
+        <a href="http://localhost:5173/Contact.jsx">Contact Us</a>
   </div>
     );
   }
